5a84feeb4d4f8919d2ffef74d707d16d2e85b8c9
Jason Nam <jasonnam@adobe.com>
2025-02-12 00:27:52 +0900

    add target.js

--- configs/target.js
+++ configs/target.js
@@ -1,5 +1,5 @@
 export const TargetConfig = {
-    targetClientCode: "",
-    imsOrg: "",
+    targetClientCode: "adobeinternalags648",
+    imsOrg: "3BC4DCE158356C820A495C37@AdobeOrg",
     decisioningMethod: "on-device"
 };
